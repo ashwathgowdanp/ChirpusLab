1169cfa0df465eac90478a0ce418b1e2e1e9d729
ashwathgowdanp <ashwathgowdanp@gmail.com>
2023-05-15 18:25:14 -0400

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'src/app/menu-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ChirpusPizzaLab';
+  title = 'Menu';
+  menuItems: MenuItem[] = [
+    {
+    name: 'Chicken Fingers',
+    category: 'Dinner',
+    price: 11.99
+    },
+    {
+      name: 'Pizza',
+      category: 'Dinner',
+      price: 11.99
+    },
+    {
+      name: "Wings",
+      category: "Sides",
+      price: 8.99
+    },
+    {
+      name: "BreadStics",
+      category: "Sides",
+      price: 4.99
+    },
+    {
+      name: "Ceasar Salad",
+      category: "Salads",
+      price: 5.99
+    },
+
+    {
+      name: "Cinnamon Roll",
+      category: "dessert",
+      price: 8.99
+    } 
+
+  ]; 
+
+  getByCategory(Cat:string): MenuItem [] {
+ 
+    
+    return this.menuItems.filter(x => x.category === Cat);
+    
+
+  }
+
+  uniqueCategories(): string[] {
+
+    return [...new Set(this.menuItems.map(item => item.category))];
+  }
+ 
 }
